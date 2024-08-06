5ba6bd6bb7187861de8c9841da9945de1877f78b
joaquin9830 <joaquin9811@gmail.com>
2024-08-06 18:39:55 -0300

    Service flowbite incorporado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { FlowbiteService } from './services/flowbite.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,13 @@ import { initFlowbite } from 'flowbite';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-  title = 'landing-angular';
+export class AppComponent implements OnInit {
+  constructor(private flowbiteService: FlowbiteService) {}
 
-  ngOnInit() {
-    initFlowbite();
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(flowbite => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
   }
 }
