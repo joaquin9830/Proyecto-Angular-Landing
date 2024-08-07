31e4cf9c1158434202073242240f5e10b0dd099c
joaquin9830 <joaquin9811@gmail.com>
2024-08-07 16:54:52 -0300

    Sección servicios terminada | html | css

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { BannerComponent } from "../../components/banner/banner.component";
 import { AboutComponent } from '../../components/about/about.component';
+import { ServicesSectionComponent } from "../../components/services-section/services-section.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, AboutComponent],
+  imports: [HeaderComponent, BannerComponent, AboutComponent, ServicesSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
