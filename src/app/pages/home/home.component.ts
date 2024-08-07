7434ae01b6f56b2d8cb1d3cedcd89c9f6bca728f
joaquin9830 <joaquin9811@gmail.com>
2024-08-07 17:33:54 -0300

    Sección contacto y footer añadidos | HTML | CSS

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,13 @@ import { BannerComponent } from "../../components/banner/banner.component";
 import { AboutComponent } from '../../components/about/about.component';
 import { ServicesSectionComponent } from "../../components/services-section/services-section.component";
 import { StockComponent } from "../../components/stock/stock.component";
+import { ContactComponent } from "../../components/contact/contact.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, AboutComponent, ServicesSectionComponent, StockComponent],
+  imports: [HeaderComponent, BannerComponent, AboutComponent, ServicesSectionComponent, StockComponent, ContactComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
