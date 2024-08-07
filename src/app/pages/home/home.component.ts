44cb16bd02e7efbafca656cac967849ff29e59cb
joaquin9830 <joaquin9811@gmail.com>
2024-08-07 17:14:59 -0300

    Sección stock terminada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { BannerComponent } from "../../components/banner/banner.component";
 import { AboutComponent } from '../../components/about/about.component';
 import { ServicesSectionComponent } from "../../components/services-section/services-section.component";
+import { StockComponent } from "../../components/stock/stock.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, AboutComponent, ServicesSectionComponent],
+  imports: [HeaderComponent, BannerComponent, AboutComponent, ServicesSectionComponent, StockComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
