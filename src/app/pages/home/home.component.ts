c0808a67f43d8cfc0ba3b9e1d4ec2f500f75d173
joaquin9830 <joaquin9811@gmail.com>
2024-08-07 16:21:03 -0300

    Sección about añadida | HTML | CSS | Favicon añadido

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { BannerComponent } from "../../components/banner/banner.component";
+import { AboutComponent } from '../../components/about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent],
+  imports: [HeaderComponent, BannerComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
